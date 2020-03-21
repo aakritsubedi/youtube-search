eed14f4e7e0913f70d30d52d1ac41d4f1655101f
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-03-21 09:51:29 +0545

    Youtube Search: Search by title

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function makeData(e, t) {
+  res = e;
+  for (var n = 0; n < t.length; n++) {
+    res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) {
+      return t[n][r];
+    });
+  }
+  return res;
+}
+
+$(function() {
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    // prepare the request
+    var request = gapi.client.youtube.search.list({
+      part: "snippet",
+      type: "video",
+      q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+      maxResults: 5,
+      order: "viewCount"
+    });
+    // execute the request
+    request.execute(function(response) {
+      var results = response.result;
+      $("#results").html("");
+      $.each(results.items, function(index,item) {
+        console.log(item);
+        
+        $.get("player/item.html", function(data) {      
+          $("#results").append(
+            makeData(data, [
+              { title: item.snippet.title, videoid: item.id.videoId, description:item.snippet.description }
+            ])
+          );
+        });
+      });
+    });
+  });
+});
+
+function init() {
+  gapi.client.setApiKey("AIzaSyClgo5jx9Ojs_T2qQ4kTrRBCvWEJmThDj8");
+  gapi.client.load("youtube", "v3", function() {
+    // YouTube api is ready
+  });
+}
